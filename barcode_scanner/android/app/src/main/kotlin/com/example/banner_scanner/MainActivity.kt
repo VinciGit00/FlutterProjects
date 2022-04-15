05e9d532bf432c8c3d489a45ee0c40d409433e8d
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2022-04-15 11:43:00 +0200

    added barcode reader

---
+++ barcode_scanner/android/app/src/main/kotlin/com/example/banner_scanner/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.banner_scanner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ barcode_scanner/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: QrCode(),
+  ));
+}
+
+class QrCode extends StatefulWidget {
+  QrCode({Key? key}) : super(key: key);
+
+  @override
+  State<QrCode> createState() => _QrCodeState();
+}
+
+class _QrCodeState extends State<QrCode> {
+  String _data = "";
+
+ _scan() async {
+   await FlutterBarcodeScanner.scanBarcode("#000000", "Cancel", true, ScanMode.BARCODE).then((value) => setState(() => _data=value));
+   
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedButton(
+              child: const Text("Scan Barcode"),
+              onPressed: ()  =>  _scan()),
+          Text(_data),
+        ],
+      ),
+    );
+  }
+}
